f133c56b1b7200c827cfa059404903f4ae8dbea3
Mitansh Joshi <mitanshjoshi@Mitanshs-MacBook-Air.local>
2025-07-04 19:17:46 +0530

    favicon

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
