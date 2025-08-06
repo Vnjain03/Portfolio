24d111d7036e3cb22f9c1b3a3144378e8efeeff7
Mitansh Joshi <mitanshjoshi@Mitanshs-MacBook-Air.local>
2025-08-06 12:06:27 +0530

    info changed

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,10 +1,10 @@
 export const contactsData = {
-    email: 'mitansh.joshi141203@gmail.com',
-    phone: '+91 8799103491',
-    address: 'Ahmedabad, Gujarat, India - 382330 ',
-    github: 'https://github.com/MitanshJoshi',
-    facebook: 'https://www.instagram.com/mitansh1412/',
-    linkedIn: 'https://www.linkedin.com/in/mitansh-joshi-317673231/',
+    email: 'vnjain03@gmail.com',
+    phone: '+91 9265911960',
+    address: 'Ahmedabad, Gujarat, India - 380004 ',
+    github: 'https://github.com/Vnjain03',
+    facebook: 'https://www.instagram.com/vanshika.03__/',
+    linkedIn: 'https://www.linkedin.com/in/vanshika-jain0822/',
     twitter: 'https://x.com/mitansh_jo21092',
 
 }
